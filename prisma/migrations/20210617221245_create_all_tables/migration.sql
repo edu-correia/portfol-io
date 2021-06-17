cb9d5f1fd9b5aa44de388170cb394f71722bbb4a
Eduardo Correia <eduardo.correia.ehsc@gmail.com>
2021-06-17 19:23:55 -0300

    Conexão com o banco de dados

---
+++ prisma/migrations/20210617221245_create_all_tables/migration.sql
@@ -0,0 +1,79 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "github" TEXT NOT NULL,
+    "linkedin" TEXT NOT NULL,
+    "location" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Project" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "repo" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "link" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Language" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "color" TEXT NOT NULL,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ProjectLanguage" (
+    "id" SERIAL NOT NULL,
+    "projectId" INTEGER NOT NULL,
+    "languageId" INTEGER NOT NULL,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Photo" (
+    "id" SERIAL NOT NULL,
+    "link" TEXT NOT NULL,
+    "caption" TEXT NOT NULL,
+    "projectId" INTEGER NOT NULL,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User.username_unique" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User.email_unique" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User.github_unique" ON "User"("github");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User.linkedin_unique" ON "User"("linkedin");
+
+-- AddForeignKey
+ALTER TABLE "Project" ADD FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProjectLanguage" ADD FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProjectLanguage" ADD FOREIGN KEY ("languageId") REFERENCES "Language"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Photo" ADD FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import dotenv from "dotenv";
 import app from "./app";
 
+dotenv.config();
 app.listen(3334, () => console.log("Server is running..."));
